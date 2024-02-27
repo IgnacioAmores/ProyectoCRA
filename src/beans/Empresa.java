7ea6417c63de5fa70bb48e3e08c30a3f830a684a
Ignacio Amores <ignacioamores@gmail.com>
2024-02-27 17:55:12 +0100

    solo apariencia
    faltan botnes, logica, bbdd

--- src/beans/Empresa.java
+++ src/beans/Empresa.java
@@ -3,13 +3,13 @@ package beans;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Datos {
+public class Empresa {
     private String codigoCuentaCotizacion;
     private String año;
     private String mes;
     private List<Trabajador> trabajadores;
 
-    public Datos(String codigoCuentaCotizacion, String año, String mes) {
+    public Empresa(String codigoCuentaCotizacion, String año, String mes) {
         this.codigoCuentaCotizacion = codigoCuentaCotizacion;
         this.año = año;
         this.mes = mes;
--- src/componentesGUI/PrincipalFrame.java
+++ src/componentesGUI/PrincipalFrame.java
@@ -1,7 +1,98 @@
 package componentesGUI;
 
-import javax.swing.JFrame;
+import java.util.List;
 
-public class PrincipalFrame extends JFrame{
-    
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+import beans.Empresa;
+import beans.Trabajador;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+public class PrincipalFrame extends JFrame {
+    private List<Empresa> datosEmpresas;
+    JPanel panelPrincipal;
+    JPanel panelDatosEmpresa;
+    JPanel panelTrabajador;
+    JPanel panelConcepto;
+    JLabel labelCodigoCuentaCotizacion;
+    JLabel labelAño;
+    JLabel labelMes;
+    JLabel labelNAF;
+    JLabel labelCodigo;
+    JLabel labelIncluidoExcluido;
+    JLabel labelImporte;
+    JTextArea textAreaCodigoCuentaCotizacion;
+    JTextArea textAreaAño;
+    JTextArea textAreaMes;
+    JTextArea textAreaNAF;
+    JTextArea textAreaCodigo;
+    JTextArea textAreaIncluidoExcluido;
+    JTextArea textAreaImporte;
+    String[] datosEmpresaLabels = {"Código de cuenta de cotización", "Año", "Mes"};
+    String[] trabajadorLabels = {"NAF"};
+    String[] conceptoLabels = {"Código", "Incluido/Excluido", "Importe"};
+
+
+
+    public PrincipalFrame() {
+        this.setTitle("Gestión de nóminas");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+
+        crearPaneles();
+        
+        this.add(panelDatosEmpresa);
+        this.add(panelTrabajador);
+        this.add(panelConcepto);
+
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+
+    private JPanel crearPanelConLabelYTextArea(String labelText, int textAreaWidth, int textAreaHeight) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        JLabel label = new JLabel(labelText);
+        JTextArea textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(textAreaWidth, textAreaHeight));
+
+        panel.add(label);
+        panel.add(textArea);
+
+        return panel;
+    }
+
+    public void crearPaneles() {
+        panelDatosEmpresa = crearPanel(datosEmpresaLabels, "Empresa", new Color(100, 149, 237));
+        panelTrabajador = crearPanel(trabajadorLabels, "Empleado", new Color(152, 251, 152));
+        panelConcepto = crearPanel(conceptoLabels, "Concepto", new Color(255, 105, 97));
+
+        panelDatosEmpresa.setVisible(true);
+        panelTrabajador.setVisible(true);
+        panelConcepto.setVisible(true);
+    }
+
+    private JPanel crearPanel(String[] labels, String titulo, Color color) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        for (String label : labels) {
+            panel.add(crearPanelConLabelYTextArea(label, 200, 20));
+        }
+
+        TitledBorder title = BorderFactory.createTitledBorder(titulo);
+        panel.setBorder(title);
+        panel.setBackground(color);
+        return panel;
+    }
 }
